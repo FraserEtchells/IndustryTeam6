eef1a3eadef2492c59c9a90674f9e85007767350
mabraham123 <33029013+mabraham123@users.noreply.github.com>
2020-10-08 19:53:19 +0100

    Js code to communicate with the python server

---
+++ Software Code/Backend/communicateWithServer.js
@@ -0,0 +1,9 @@
+//Standard commands recognised by the servers
+const NAME= "NAME";
+const JOIN= "JOIN";
+const HOST= "HOST";
+const CODE= "CODE";
+const CHAT= "CHAT";
+const POINTS= "POINTS";
+const QUIT= "QUIT";
+const SCORE="SCORE";
