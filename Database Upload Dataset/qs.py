4572159add398167d018522626d3ca3a6def8771
romanbro <37274830+romanbro@users.noreply.github.com>
2020-10-08 15:41:20 +0100

    Add files via upload
    fully fixed the ability to read all the faux characters that are present in the original text files

--- Database Upload Dataset/qs.py
+++ Database Upload Dataset/qs.py
@@ -1,5 +1,6 @@
 from mongodb_insertmany import upload
-import random, os
+import random, os, ftfy
+
 
 ##searches the categories folder and returns all the files in there (they contain questions)
 def getQuestionFiles():
@@ -15,12 +16,12 @@ def getQuestions(filename, questions, questionLimit):
     if questionLimit == -1: questionLimit = 1000000000
 
     if verbose:
-        file = open("categories\\" + filename, "r", encoding="latin_1")
+        file = open("categories\\" + filename, "r", encoding="latin-1")
         print("question file: \t\t\t " + filename)
         print("number of counted questions: \t", file.read().count("#Q"))
         file.close()
     
-    file = open("categories\\" + filename, "r", encoding="latin_1")
+    file = open("categories\\" + filename, "r", encoding="latin-1")
     rawQuestions = file.readlines()
 
     numberOfNewQuestions = 0
@@ -28,13 +29,15 @@ def getQuestions(filename, questions, questionLimit):
     for i in range(len(rawQuestions)):
         if len(questions) >= questionLimit: break
 
-        question = {"question":     "blank",                                                        ##question dictionary
+        question = {"id":           "blank",                                                        ##question dictionary
+                    "question":     "blank",                                                        
                     "correct_ans":  "blank",
                     "answer_a":     "blank",
                     "answer_b":     "blank",
                     "answer_c":     "blank",
                     "answer_d":     "blank",
                     "category":     "blank"}
+
         
         question["question"] = rawQuestions[i]                                                      ##reads in the question string
         if question["question"] == "": question["question"] = "@"                                   ##checks if the string is empty and if so, replaces it with "@"
@@ -68,7 +71,13 @@ def getQuestions(filename, questions, questionLimit):
 
         question["category"] = filename.replace(".txt", "")                                         ##the category is the filename without the extension
 
+        question["id"] = numberOfNewQuestions
 
+
+        for k, v in question.items():
+            if k != "id" and k != "category":
+                question[k] = ftfy.fix_encoding(v)                                                  ##fixes all the messed up foreign characters, thank god
+        
         i += 5
         numberOfNewQuestions += 1
         questions.append(question)                                                                  ##after each string gets checked and cleaned up the question dictionary then gets submitted to the questions array
@@ -88,64 +97,20 @@ def getAllQuestions(questionLimit):
     if verbose: print("total questions: \t\t", len(allQuestions), "\n")
 
     return allQuestions
-    
 
+
+    
 ##iterates through the questions and outputs them to the console
 def printQuestions(questions):
     for i in range(len(questions)):
         for k, v in questions[i].items():
-            if k == "question": print(str(i+1) + ") ", end="")
+##            if k == "question":
+##                print(str(i+1) + ") ", end="")
             print(v)
 
         print()
         print()
 
-
-##writes to SQL file compatible with MySQL
-def writeSQL(questions):
-    sqlfile = open("qs.sql", "w", encoding="latin_1")
-    tablename = "Table"
-    sqlfile.write("INSERT INTO " + tablename + " (question, correct_ans, answer_a, answer_b, answer_c, answer_d)\n")
-    sqlfile.write("VALUES \n(\n")
-    for i in range(len(questions)):
-        counter = 1
-        line = "    ("
-        for k, v in questions[i].items():
-            line += ("'" + v + "', ")
-
-            counter += 1
-            if counter == 7:
-                if i == len(questions) - 1: ##last question
-                    sqlfile.write(line[:-2] + ")")
-                    sqlfile.write("\n);")
-                else:
-                    sqlfile.write(line[:-2] + "),\n")
-                    counter = 1
-    sqlfile.close()
-
-
-##writes to JSON file compatabile with MongoDB
-def writeJSON(questions):
-    jsonfile = open("qs.json", "w", encoding="latin_1")
-    jsonfile.write("db.inventory.insertMany([\n")
-    for i in range(len(questions)):
-        counter = 1
-        line = "    {"
-        for k, v in questions[i].items():
-            
-
-            line += ("'" + k + "': '" + v + "', ")                
-                
-
-            counter += 1
-            if counter == 7:
-                if i == len(questions) - 1: ##last question
-                    jsonfile.write(line[:-2] + "}")
-                    jsonfile.write("\n]);")
-                else:
-                    jsonfile.write(line[:-2] + "},\n")
-                    counter = 1
-    jsonfile.close()   
 
 
 ##returns a given amount of random questions
@@ -153,9 +118,12 @@ def selectSomeQuestions(allQuestions, amount):
     indexes = random.sample(range(len(allQuestions)), amount)
     
     someQuestions = []
+    count = 1
     for i in indexes:
         if i == []: break
         someQuestions.append(allQuestions[i])
+        someQuestions[count - 1]["id"] = count
+        count += 1
 
     return someQuestions
 
@@ -163,17 +131,17 @@ def selectSomeQuestions(allQuestions, amount):
 ##main
 def main():
     allQuestions = getAllQuestions(questionLimit)
-    someQuestions = selectSomeQuestions(allQuestions, numberOfQuestions)    
-    upload(someQuestions, output, verbose)
+    someQuestions = selectSomeQuestions(allQuestions, numberOfQuestions)
+##    upload(someQuestions, output, verbose)
 
-
+##    printQuestions(someQuestions)
+##    print(printQuestions(getQuestions("q.txt", [], 100)))
 
 if __name__ == "__main__":
-    
     ##global variables
     verbose = True              ##prints some extra info to the console
-    output = True               ##prints the data of the uploaded questions to the console
-    numberOfQuestions = 100     ##the number of questions to upload to the database
+    output = False               ##prints the data of the uploaded questions to the console
+    numberOfQuestions = 500     ##the number of questions to upload to the database
     questionLimit = -1          ##maximum amount of questions to read from one file (-1 is inf)
     
     main()
