365aca76b89c07ab6c2cf2d879655521d6466075
RossMitchell1999 <rdzmitchell@dundee.ac.uk>
2020-10-09 15:46:43 +0100

    Addition of database info retrieval

--- Software Code/Backend/Lobby Server/ServerCode.py
+++ Software Code/Backend/Lobby Server/ServerCode.py
@@ -2,6 +2,7 @@
 #Ross Mitchell
 
 import selectors
+from pymongo import MongoClient # library for connecting with MongoDB
 import socket
 from datetime import datetime
 import platform
@@ -9,7 +10,7 @@ import re
 import types
 import random
 import string
-
+from pprint import pprint
 platform.node()
 HEADERSIZE = 10
 
@@ -129,6 +130,65 @@ def score(command, p, code):
     for i in Lobbies[code].values():
         i.sock.send(message.encode())
 
+def getQuestion(command, p, code):
+    # connect to MongoDB
+    dburi = "mongodb://team6-mongodb:4LITWMsMLAzi1w4rZbuOo0wgaaUlFk0nO3WMj1riXjsnL0rkZqmRgeX0oVnWTHOhlOgr7NX6H97S00pwfgWxlA==@team6-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@team6-mongodb@"
+    client = MongoClient(dburi)
+
+    my_db = client["questions"] # db name
+
+    my_col = my_db["questions"] # db collection
+
+    numQuests = 10 # number of questions to get
+    questIDs = []
+    max_ID = (my_col.count_documents({})) + 1
+
+    pprint("Max number is: ")
+    pprint(max_ID)
+
+    # while loop to get the required number of random IDs, while also not accepting duplicates
+    num = 0
+    while num < numQuests:
+        newNum = False
+        newRand = 0
+        while newNum != True:
+            newRand = random.choice(range(1, max_ID))
+            temp = 0
+            while temp < len(questIDs):
+                if newRand != questIDs[temp]:
+                    newNum = True
+                    break
+                temp = temp + 1
+            questIDs.append(newRand)
+            num = num + 1
+
+    # print the IDs generated
+    pprint(questIDs)
+
+    # depreciated - create a list to hold the questions & get them from database
+    # tempQuests = []
+    # tempQuests = my_col.find({}, {"_id":0})
+    # pprint(tempQuests)
+
+    # create a list to hold questions & get from database, transfer the questions we want (according to IDs generated) into a new list & get rid of original
+    randQuests = []
+    for ID in questIDs:
+        quest = list(my_col.find({"id" : int(ID)}, {"_id":0}))
+        randQuests.append(quest)
+        # randQuests.append(tempQuests[ID])
+    # del tempQuests
+
+    # print the questions that have been selected
+    for quest in randQuests:
+        pprint(quest)
+
+    question = randQuests
+    message = "QUESTIONS"
+
+    for i in Lobbies[code].values():
+        i.sock.send(message.encode())
+        i.sock.send(question.encode())
+
 #This is for the buffer accepting the connectio
 def accept_wrapper(sock):
     #This is the socket accepting the connection after listening
@@ -228,6 +288,11 @@ def service_connection(key, mask):
                     command = data.outb.decode()
                     #calls the function for leaving the channel
                     score(command, pointer, code)
+                elif "QUESTION" in repr(data.outb):
+                    #decodes the command
+                    command = data.outb.decode()
+                    #calls the function for leaving the channel
+                    getQuestion(command, pointer, code)
                 #checks that the socket hasnt closed
                 if i==0:
                     #sends item to the sockets
