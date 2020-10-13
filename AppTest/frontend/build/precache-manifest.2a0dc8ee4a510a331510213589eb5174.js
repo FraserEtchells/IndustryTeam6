9634e3b752b2546db5eefba224106fb3005cba72
Fraser Etchells <fraseretchells1@hotmail.co.uk>
2020-10-13 16:13:08 +0100

    Added webconfig and various changes

---
+++ AppTest/frontend/build/precache-manifest.2a0dc8ee4a510a331510213589eb5174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15c483830f86047d775caa2728e125d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc25cda1484ef1a1ccdb",
+    "url": "/static/js/2.4d77bde1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4d77bde1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469ccb2f8970273afc39",
+    "url": "/static/js/main.2d42e728.chunk.js"
+  },
+  {
+    "revision": "89ac35b4ba8ca899f16a",
+    "url": "/static/js/runtime-main.7f7132dd.js"
+  },
+  {
+    "revision": "a906773f4b26fc32eceff968ff5c4ffc",
+    "url": "/static/media/logo.a906773f.svg"
+  }
+]);
