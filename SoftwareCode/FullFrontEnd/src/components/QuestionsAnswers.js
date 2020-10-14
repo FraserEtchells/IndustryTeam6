dac804c8517280b62ed825e6d74897062ef178a7
Kayla <ktwallace511@gmail.com>
2020-10-14 10:39:35 +0100

    Routing WIP
    Basic routing mostly done through a navbar at the moment (this will not be featured in final product). Still a few pages to be added.

---
+++ SoftwareCode/FullFrontEnd/src/components/QuestionsAnswers.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from "styled-components";
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+ @import url('https://fonts.googleapis.com/css2?family=Passion+One&display=swap');
+  body {
+    background-color:  #7E549F;
+    font-family: 'Passion One', cursive;
+    overflow: hidden;
+    text-align: center;
+    font: 50px;
+  }
+`;
+
+// grid width can be changed depending on how big the other components are
+export const Grid = styled.div`
+  background-color: #7E549F; // purple
+  width: 940px;
+`;
+
+
+//display: flex; stretches it out a bit vertically but then the text doesn't center align
+//display: block; FIXES IT <3
+
+//adding height stops it from stretching vertically
+//but seems to put it in the top 'line' so if it's a long question it'll wrap to the line below
+//so if it's a short one it leaves a gap for the line underneath
+export const QuestionRow = styled.div`
+  display: block;
+  background-color: #CAB1DE; // lilac
+  justifyContent: center;
+  alignItems: center;
+  margin:;
+  padding: 5px 5px;
+  text-align: center;
+  height:px;
+`;
+
+export const QuestionText = styled.h2`
+
+`;
+
+export const Row = styled.div`
+  display: flex;
+
+  justifyContent:center;
+    alignItems: center;
+`;
+
+export const Col = styled.div`
+  flex: ${(props) => props.size};
+`;
+
+// button width used to be 400px but changed to percentage so it resizes
+
+const Button = styled.button`
+@import url('https://fonts.googleapis.com/css2?family=Passion+One&display=swap');
+    font-family: 'Passion One', cursive;
+    color: white;
+    border: none;
+    width: 50%;
+    height: 200px;
+    font-size: 20px;
+    margin-top: 10px;
+    margin-bottom: 5px;
+    margin-left: 5px;
+    margin-right: 0px;
+    outline: none;
+    
+    :hover {
+      text-decoration: underline;
+      font-size: 2.5em;
+    }
+`;
+
+const Button1 = styled(Button)` 
+  background: #FB836F; // orange
+  `;
+
+const Button2 = styled(Button)`
+  background: #4E937A; // green  
+  `;
+
+const Button3 = styled(Button)`
+  background: #E3B538; // yellow
+  `;
+
+const Button4 = styled(Button)`
+  background: #C1549C; // pink
+  `;
+
+// button border and colour if we decide to use it
+//  border: 2px solid #a90081;
+
+// want to use this colour in background but not sure how outside of divs
+//  background-color: #7E549F;
+
+class Answers extends React.Component {
+  state = {
+
+  };
+
+  render() {
+    return (
+    <React.Fragment>
+    <GlobalStyle/>
+    <div className="App">
+
+      <Grid>
+        <QuestionRow>
+          <QuestionText>
+            QUESTION QUESTION QUESTION QUESTION?
+          </QuestionText>
+        </QuestionRow>
+
+        <Row>
+          <Button1>ANSWER</Button1>
+          <Button2>ANSWER</Button2>
+        </Row>
+        <Row>
+
+          <Button3>ANSWER</Button3>
+          <Button4>ANSWER</Button4>
+       </Row>
+      </Grid>
+
+    </div>
+
+    </React.Fragment>
+    );
+  }
+}
+
+export default Answers;
---
+++ SoftwareCode/FullFrontEnd/src/main.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter
+  } from "react-router-dom";
+  import MainMenu from "./main-menu";
+  import CustomisationPage from "./index-custom-page";
+  import QuizPage from "./quiz-page";
+  import styled from 'styled-components';
+
+class Main extends Component {
+  render() {
+    return (
+        <HashRouter>
+            <div>
+                <ul className="header">
+                    <li><NavLink to="/main-menu">Home</NavLink></li>
+                    <li><NavLink to="/index-custom-page">Customisation</NavLink></li>
+                    <li><NavLink to="/quiz-page">Quiz</NavLink></li>
+                </ul>
+                <div className="content">
+                    <Route exact path="/main-menu" component={MainMenu}/>
+                    <Route path="/index-custom-page" component={CustomisationPage}/>
+                    <Route path="/quiz-page" component={QuizPage}/>
+                </div>
+            </div>
+        </HashRouter>
+    );
+  }
+}
+ 
+export default Main;
