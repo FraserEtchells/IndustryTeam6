621d97c6b1625b7777e290cbdc51b6630775f8ee
RuthC <ruthc99@rocketmail.com>
2020-10-14 13:04:30 +0100

    Fixed font and BG

--- SoftwareCode/FullFrontEnd/src/components/Customisation.js
+++ SoftwareCode/FullFrontEnd/src/components/Customisation.js
@@ -55,7 +55,7 @@ const ColourHead = styled.div`
 const ColourHeader = () => {
   return (
     <ColourHead>
-      <h2>PLAYERS CUSTOMISATIONS</h2>
+      <h2>PLAYER CUSTOMISATION</h2>
     </ColourHead>
   );
 };
