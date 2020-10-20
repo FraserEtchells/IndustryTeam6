ecb2216642d0abeee8895976827265b779150821
Fraser Etchells <fraseretchells1@hotmail.co.uk>
2020-10-20 10:35:58 +0100

    New branch that holds up to date code

---
+++ SoftwareCode/QuizWithLeaderboard/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Quiz from './Components/Quiz';
+
+function App() {
+  return (
+    <React.Fragment>
+ 
+    <Quiz />
+    </React.Fragment>
+    
+  );
+}
+
+export default App;
+
+
